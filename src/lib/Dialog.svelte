39e4bb43a2ab3e8bd9ef641198f4245140d120d5
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-06-02 10:16:42 +0700

    Add component: Dialog

---
+++ src/lib/Dialog.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import type * as Leaflet from 'leaflet';
+	import type { MapEvent, PickLocationEvent } from './types';
+	import { createEventDispatcher } from 'svelte';
+	import Picker from './Picker.svelte';
+
+	export let cancelText = 'Cancel';
+	export let selectText = 'Select';
+
+	const dispatch = createEventDispatcher<{ select: { picked: Leaflet.LatLngTuple } }>();
+	let dialog: HTMLDialogElement;
+	let map: Leaflet.Map;
+	let selected: Leaflet.LatLngTuple;
+
+	$: selectedString = selected ? selected.join(',') : '';
+
+	function close() {
+		dialog.close(selectedString);
+	}
+
+	function open() {
+		dialog.show();
+		map.invalidateSize();
+	}
+
+	function handleMapReady(event: CustomEvent<MapEvent>) {
+		map = event.detail.map;
+	}
+
+	function handlePick(e: CustomEvent<PickLocationEvent>) {
+		({ picked: selected } = e.detail);
+	}
+
+	function handleSelect() {
+		dispatch('select', { picked: selected });
+		close();
+	}
+</script>
+
+<dialog class="lp-dialog" bind:this={dialog} on:close>
+	<slot name="header" />
+	<Picker {...$$restProps} on:pick={handlePick} on:ready={handleMapReady} />
+	<div class="lp-dialog-buttons">
+		<button
+			class="lp-dialog-button-select"
+			disabled={!Boolean(selected)}
+			type="button"
+			value={selectedString}
+			on:click|preventDefault={handleSelect}
+		>
+			{selectText}
+		</button>
+		<button class="lp-dialog-button-cancel" type="button" on:click|preventDefault={close}>
+			{cancelText}
+		</button>
+	</div>
+</dialog>
+
+<slot name="trigger" {open} />
+<slot name="result" {selected} />
+
+<style>
+	dialog {
+		z-index: 99999;
+		padding: unset;
+		width: 96vw;
+		height: 90vh;
+	}
+
+	dialog[open] {
+		display: flex;
+		flex-direction: column;
+	}
+
+	dialog :global(.lp-map) {
+		flex: 1;
+	}
+</style>
