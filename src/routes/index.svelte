e0050cdbe26e7906def63cb75a65976eaeaf9269
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-06-02 07:40:27 +0700

    Add simple picker example to homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Picker from '$lib/Picker.svelte';
+
+	let example_1 = '';
+</script>
+
+<svelte:head>
+	<title>Location Picker</title>
+</svelte:head>
+
+<h1>Location Picker</h1>
+
+<div>
+	<Picker on:pick={(e) => (example_1 = e.detail.picked.join(', '))} />
+	<p>
+		<input readonly type="text" value={example_1} />
+	</p>
+</div>
+
+<style>
+	input {
+		width: 100%;
+		font-variant-numeric: oldstyle-nums;
+		letter-spacing: 0.025rem;
+	}
+</style>
