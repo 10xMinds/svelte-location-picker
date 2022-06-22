923af141eea771abc1eb950fc9c5e829c91925ac
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-06-22 22:55:08 +0700

    Export Modal & Popup components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 export { default as Dialog } from './Dialog.svelte';
 export { default as MapBox } from './MapBox.svelte';
+export { default as Modal } from './Modal.svelte';
 export { default as Picker } from './Picker.svelte';
+export { default as Popup } from './Popup.svelte';
 export { toLatLng } from './utils';
